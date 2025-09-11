245e6f4429a8afdb204522901515969eb8fbfcc8
albert <albertshin5172@gmail.com>
2025-09-11 21:56:09 +0900

    feat : first commit

---
+++ src/Test.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/*
+1. The name of a component must begin with an uppercase letter.
+2. The component must be exported so that it can be used by other components.
+*/
+function Test() {
+  return (
+    <div>
+      <h1>This is a Test Component!</h1>
+    </div>
+  );
+}
+
+export default Test;
