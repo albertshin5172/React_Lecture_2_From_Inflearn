6f08cd1f5bb0598dcc434d476aa655fb4c96405c
albert <albertshin5172@gmail.com>
2025-09-11 22:03:12 +0900

    feat : second test commit

--- src/Test.js
+++ src/Test.js
@@ -3,10 +3,15 @@ import React from "react";
 /*
 1. The name of a component must begin with an uppercase letter.
 2. The component must be exported so that it can be used by other components.
+3. If a component wants to use another component, it must import it.
 */
 function Test() {
   return (
     <div>
+      <h1>This is a Test Component!</h1>
+      <h1>This is a Test Component!</h1>
+      <h1>This is a Test Component!</h1>
+      <h1>This is a Test Component!</h1>
       <h1>This is a Test Component!</h1>
     </div>
   );
