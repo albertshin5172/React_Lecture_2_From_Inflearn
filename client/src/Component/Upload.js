5551b8af90d902d950be19ee307b5b734296bb39
albert <albertshin5172@gmail.com>
2025-09-15 23:32:07 +0900

    feat : section2 React - 18. Emotion

--- client/src/Component/Upload.js
+++ client/src/Component/Upload.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { UploadDiv, UploadForm, UploadButtonDiv } from "../Style/UploadCSS.js";
 
 function Upload(props) {
   const [Content, setContent] = useState("");
@@ -23,32 +24,57 @@ function Upload(props) {
   }, [Content]);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100%",
-        marginTop: "1rem",
-      }}
-    >
-      <input
-        type="text"
-        value={Content}
-        onChange={(e) => {
-          console.log(e.currentTarget.value);
-          setContent(e.currentTarget.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          onSubmit();
-        }}
-        style={{ marginTop: "1rem" }}
-      >
-        SUBMIT!
-      </button>
-    </div>
+    <UploadDiv>
+      <UploadForm>
+        <label htmlFor="">Title</label>
+        <input
+          id="title"
+          type="text"
+          value={Content}
+          onChange={(e) => {
+            console.log(e.currentTarget.value);
+            setContent(e.currentTarget.value);
+          }}
+        />
+        <label htmlFor="">Content</label>
+        <textarea />
+        <UploadButtonDiv>
+          <button
+            onClick={(e) => {
+              onSubmit(e);
+            }}
+          >
+            SUBMIT!
+          </button>
+        </UploadButtonDiv>
+      </UploadForm>
+    </UploadDiv>
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     alignItems: "center",
+    //     width: "100%",
+    //     marginTop: "1rem",
+    //   }}
+    // >
+    //   <input
+    //     type="text"
+    //     value={Content}
+    //     onChange={(e) => {
+    //       console.log(e.currentTarget.value);
+    //       setContent(e.currentTarget.value);
+    //     }}
+    //   />
+    //   <button
+    //     onClick={() => {
+    //       onSubmit();
+    //     }}
+    //     // style={{ marginTop: "1rem" }}
+    //   >
+    //     SUBMIT!
+    //   </button>
+    // </div>
   );
 }
 
