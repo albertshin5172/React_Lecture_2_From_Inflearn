eb9165646e88a137bf720a84ff24e90cfa8630b9
albert <albertshin5172@gmail.com>
2025-09-18 18:56:24 +0900

    feat : section2 React - 33. Login: Implementing login

--- client/src/Component/User/Login.js
+++ client/src/Component/User/Login.js
@@ -1,14 +1,43 @@
 import React, { useState, useEffect } from "react";
-// import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { LoginDiv } from "../../Style/UserCSS.js";
-
-// import firebase from "../../firebase.js";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase.js";
 
 function Login() {
   const [Email, setEmail] = useState("");
   const [PW, setPW] = useState("");
+  const [ErrorMsg, setErrorMsg] = useState("");
   let navigate = useNavigate();
+
+  const SingInFunc = async (e) => {
+    e.preventDefault();
+    if (!(Email && PW)) {
+      return alert("Please fill in all values.");
+    }
+
+    try {
+      //await firebase.auth().signInWithEmailAndPassword(Email, PW);
+      await signInWithEmailAndPassword(auth, Email, PW);
+      //await updateProfile(createdUser.user, { displayName: Name });
+      navigate("/");
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === "auth/user-not-found") {
+        setErrorMsg("This email address does not exist.");
+      } else if (error.code === "auth/wrong-password") {
+        setErrorMsg("Password does not match.");
+      } else {
+        setErrorMsg("Password does not match.");
+      }
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setErrorMsg("");
+    }, 5000);
+  }, [ErrorMsg]);
 
   return (
     <LoginDiv>
@@ -20,13 +49,15 @@ function Login() {
           required
           onChange={(e) => setEmail(e.currentTarget.value)}
         />
+        <label>비밀번호</label>
         <input
           type="password"
           value={PW}
           required
           onChange={(e) => setPW(e.currentTarget.value)}
         />
-        <button>로그인</button>
+        {ErrorMsg !== "" && <p>{ErrorMsg}</p>}
+        <button onClick={(e) => SingInFunc(e)}>로그인</button>
         <button
           onClick={(e) => {
             e.preventDefault();
@@ -40,38 +71,8 @@ function Login() {
   );
   // const [Email, setEmail] = useState("");
   // const [PW, setPW] = useState("");
-  // const [ErrorMsg, setErrorMsg] = useState("");
-  // const user = useSelector((state) => state.user);
   // let navigate = useNavigate();
-  // const SingInFunc = async (e) => {
-  //   e.preventDefault();
-  //   if (!(Email && PW)) {
-  //     return alert("모든 값을 채워주세요.");
-  //   }
-  //   try {
-  //     await firebase.auth().signInWithEmailAndPassword(Email, PW);
-  //     navigate("/");
-  //   } catch (error) {
-  //     console.log(error.code);
-  //     if (error.code === "auth/user-not-found") {
-  //       setErrorMsg("존재하지 않는 이메일입니다.");
-  //     } else if (error.code === "auth/wrong-password") {
-  //       setErrorMsg("비밀번호가 일치하지 않습니다.");
-  //     } else {
-  //       setErrorMsg("로그인이 실패하였습니다.");
-  //     }
-  //   }
-  // };
-  // useEffect(() => {
-  //   if (user.accessToken) {
-  //     navigate("/");
-  //   }
-  // }, []);
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setErrorMsg("");
-  //   }, 5000);
-  // }, [ErrorMsg]);
+
   // return (
   //   <LoginDiv>
   //     <form>
@@ -82,15 +83,13 @@ function Login() {
   //         required
   //         onChange={(e) => setEmail(e.currentTarget.value)}
   //       />
-  //       <label>비밀번호</label>
   //       <input
   //         type="password"
   //         value={PW}
   //         required
   //         onChange={(e) => setPW(e.currentTarget.value)}
   //       />
-  //       {ErrorMsg != "" && <p>{ErrorMsg}</p>}
-  //       <button onClick={(e) => SingInFunc(e)}>로그인</button>
+  //       <button>로그인</button>
   //       <button
   //         onClick={(e) => {
   //           e.preventDefault();
