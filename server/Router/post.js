16f5377b0a97b768b83a2d91cd186eb94b5b450f
albert <albertshin5172@gmail.com>
2025-09-19 18:58:46 +0900

    feat : Correct image upload error handling 2

--- server/Router/post.js
+++ server/Router/post.js
@@ -83,6 +83,7 @@ router.post("/edit", (req, res) => {
   let temp = {
     title: req.body.title,
     content: req.body.content,
+    image: req.body.image,
   };
   Post.updateOne({ postNum: Number(req.body.postNum) }, { $set: temp })
     .exec()
