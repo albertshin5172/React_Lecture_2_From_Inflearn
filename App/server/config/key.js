3567e143f57fb1ab5e1477aa06eed0c0255ce058
albert <albertshin5172@gmail.com>
2025-09-21 19:06:43 +0900

    feat : modify key.js

--- App/server/config/key.js
+++ App/server/config/key.js
@@ -1,7 +1,12 @@
-if (process.env.NODE_ENV === "production") {
-  //배포상태
-  module.exports = require("./production.js");
-} else {
-  //개발상태
-  module.exports = require("./dev.js");
-}
+// if (process.env.NODE_ENV === "production") {
+//   //배포상태
+//   module.exports = require("./production.js");
+// } else {
+//   //개발상태
+//   module.exports = require("./dev.js");
+// }
+// server/config/key.js
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.JWT_SECRET,
+};
